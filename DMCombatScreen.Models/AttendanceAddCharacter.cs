4865f7b0a230abbe06eb257df4f00c7b97b415a1
drewawright <drewawright@gmail.com>
2019-07-18 15:36:41 -0400

    added tooltips to roll initiative screen

--- DMCombatScreen.Models/AttendanceAddCharacter.cs
+++ DMCombatScreen.Models/AttendanceAddCharacter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace DMCombatScreen.Models
     public class AttendanceAddCharacter
     {
         public int CombatID { get; set; }
+        [Display(Name="Combat Name")]
         public string CombatName { get; set; }
         public List<AttendanceCharacterInfo> CharacterList { get; set; }
     }
